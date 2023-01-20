fdefb14dc94e8c25587f42269f8f5296f207e58f
Michal Szczepanski <michal@vane.pl>
2023-01-20 17:11:54 +0100

    fix: jison resolve path

--- src/JisonResolver.js
+++ src/JisonResolver.js
@@ -1,17 +1,25 @@
 import {Resolver} from '@parcel/plugin';
-import fs from 'fs';
+import path from 'path';
 import jison from 'jison';
 
 export default new Resolver({
-  async resolve({specifier}) {
-    console.log(specifier);
-    if(fs.existsSync(specifier)) {
-      const source = await fs.readFile(specifier)
-      const parser = new jison.Jison.Parser(source);
-      return {
-        filePath: specifier,
-        code: parser.generate()
-      };
+  async resolve({specifier, options, dependency}) {
+    const exists = await options.inputFS.exists(specifier)
+    if (!exists && dependency) {
+      const a = dependency.resolveFrom.split('/');
+      const basepath = a.slice(0, a.length - 1).join('/');
+      const specpath = specifier + '.jison'
+      const fpath = path.join(basepath, specpath);
+      const existsJison = await options.inputFS.exists(fpath);
+      if (existsJison) {
+        const source = await options.inputFS.readFile(fpath);
+        const sourceString = source.toString();
+        const parser = new jison.Jison.Parser(sourceString);
+        return {
+          filePath: fpath,
+          code: parser.generate()
+        }
+      }
     }
     return null;
   }
